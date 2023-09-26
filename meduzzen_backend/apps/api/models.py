0e0e65cdf9579a0526dac940f9dc33c1247a6933
hifichevymane <blekbjek@gmail.com>
2023-09-26 20:31:18 +0300

    Edit TimeStampedModel to be abstract. Delete migrations folder in /api folder.

--- meduzzen_backend/apps/api/models.py
+++ meduzzen_backend/apps/api/models.py
@@ -1,16 +1,12 @@
-import logging
 from django.db import models
 
-# Logging all models actions
-logger = logging.getLogger('main')
-
-
 # Create your models here.
+# Abstract model TimeStampedModel
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # Adding a record
-    def save(self, *args, **kwargs):
-        logger.info('The record has been added')
-        super(TimeStampedModel, self).save(*args, **kwargs)
+    class Meta:
+        # abstract = True to enable models inherit from TimeStampedModel
+        # https://www.geeksforgeeks.org/how-to-create-abstract-model-class-in-django/
+        abstract = True
