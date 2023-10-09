01c2c12a29221c3a6e93fa3c6d83e83b0290d210
hifichevymane <blekbjek@gmail.com>
2023-10-09 19:37:47 +0300

    Try to fix pytest bug in Github actions

--- meduzzen_backend/meduzzen_backend/settings.py
+++ meduzzen_backend/meduzzen_backend/settings.py
@@ -234,7 +234,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT'),
         'TEST': {
             'NAME': 'test'
