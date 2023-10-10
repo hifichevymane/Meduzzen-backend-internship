a86090a40a0f69911737db5ea9fb0009bdea54be
hifichevymane <blekbjek@gmail.com>
2023-10-10 12:11:29 +0300

    Try to fix db connection bug

--- meduzzen_backend/meduzzen_backend/settings.py
+++ meduzzen_backend/meduzzen_backend/settings.py
@@ -234,8 +234,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'HOST': 'localhost',
+        'PORT': 5432,
         'TEST': {
             'NAME': 'test'
         }
