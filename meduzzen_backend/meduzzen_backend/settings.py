fb607b163d54ad6f84da9d0d43e11a64cc060d1f
hifichevymane <blekbjek@gmail.com>
2023-10-10 12:29:45 +0300

    Try to fix db connection error github actions

--- meduzzen_backend/meduzzen_backend/settings.py
+++ meduzzen_backend/meduzzen_backend/settings.py
@@ -234,8 +234,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'postgres',
-        'PORT': 5432,
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
         'TEST': {
             'NAME': 'test'
         }
